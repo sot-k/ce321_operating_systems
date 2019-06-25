a83f26568ee1d3ff75d73ec1e30a47c96d501c6c
sot-k <52216758+sot-k@users.noreply.github.com>
2019-06-25 22:39:21 +0300

    Delete test.c

--- operating systems/Project3/Project3/project_3_omada_4/test.c
+++
@@ -1,7 +0,0 @@
-#include "syscall_wrapper.h"
-#include <stdio.h>
-
-int main (int argc, char *argv[]){
-	printf("Allocated Memory: %ld.\n Free Memory: %ld.\n", alloc_mem_syscall_wrapper(), free_mem_syscall_wrapper())
-}
-
